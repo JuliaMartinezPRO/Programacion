6e7e80fcee3d2a1e973f04200a7f333db9c54aba
IESPIRINGALLA\marfonjul <juliamartinezfontao@gmail.com>
2023-11-17 21:08:53 +0100

    fibonacci

---
+++ ExamenUD2/fibonacci.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ExamenUD2;
+import java.util.Scanner;
+/**
+ *
+ * @author marfonjul
+ */
+public class fibonacci {
+    public static void main(String arg[]){
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       int numero1=0;
+       int numero2=1;
+       for(int x=2;x<=n;x++){
+           int term=numero1;
+           int res = numero1+numero2;
+           numero1=term;
+       }
+       System.out.print("resultado"+n);
+    }
+    
+}
---
+++ ExamenUD2/juliaexamenUD2.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ExamenUD2;
+import java.util.Scanner;
+/**
+ *
+ * @author marfonjul
+ */
+public class juliaexamenUD2 {
+    public static void main (String arg[]){
+        int opcionUno=0;
+        int opcion2=0;
+        int opcion3=0;
+           System.out.println("Bienvenido al examen de la UD2-estructuras de control.Seleccione la opción deseada"+opcionUno+opcion2+opcion3);
+           
+           
+     //OPCION2
+     //Calacula el cuadrado de los 10 primeros numneros pares
+     int n=0;
+     int resultado =1;
+    while(n<10){
+        if(n%2==0);
+        resultado=n*n;
+    }
+    n++;
+     System.out.println("resultado"+n);
+     
+     
+     //OPCION3
+     //creamos el objeto Scanner
+     Scanner scanner = new Scanner (System.in);
+     //pedir al usuario que ingrese el numero
+     System.out.println("Ingrese un número");
+     int n = scanner.nextInt ();
+     int num1=0;
+     int num2=1;
+     int suma = 0;
+     for(int x=num1;x<=num2;x++){
+         suma += suma*n;
+     }
+     System.out.println("resultado"+n);
+     //OPCION1
+     int numero1=2;
+     int numero2=3;
+     int multiplicar=0;
+     for (int y=1;y<=2;y++){
+        int resultado*=(int)math.pow(2,y);
+         multiplicar = numero1*numero2;
+     }
+           System.out.println("resultado"+multiplicar);
+    }
+}
