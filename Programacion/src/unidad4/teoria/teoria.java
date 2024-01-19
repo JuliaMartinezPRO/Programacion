f9b78a76d1d47e700b5e6d39d2daba54345e86e0
IESPIRINGALLA\marfonjul <juliamartinezfontao@gmail.com>
2024-01-19 21:33:07 +0100

    ...

--- Programacion/src/unidad4/teoria/teoria.java
+++ Programacion/src/unidad4/teoria/teoria.java
@@ -41,4 +41,24 @@ public class Teoria {
         }
         System.out.println("el estado de salud es :" + estado);
     }
+    //arrays multidimensionales
+    double tabla1 [][] = new  double [3][2]; // 3 filas y dos columnas
+    int tabla2 [][] = new int [2][]; //2 filas y varias columnas
+    tabla2[0] = new int[2]; //primera fila con 2 columnas
+    tabla2[1] = new int[3]; //segunda fila con 3 columnas
+    
+    System.out.println(tabla1.length);//nº de filas
+    System.out.println(tabla2.length);//nº de filas
+    System.out.println(tabla2[0].length);//nº de columnas
+    System.out.println(tabla2[1].length);//nº de columnas
+    
+    //otro ejemplo
+    private int sumaCol(int[][] matriz, int columna){
+        int suma = 0;
+        for (int fila = 0; fila < matriz.length; fila++){
+            suma += matriz[fila][columna];
+            return suma;
+        }
+        return 0;
+    }
 }
