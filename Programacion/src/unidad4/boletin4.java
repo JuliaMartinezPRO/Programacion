1d6673da510cb65b243e909b1d75991afec068ab
IESPIRINGALLA\marfonjul <juliamartinezfontao@gmail.com>
2024-01-19 22:27:42 +0100

    arrays

--- Programacion/src/unidad4/boletin4.java
+++ Programacion/src/unidad4/boletin4.java
@@ -4,11 +4,36 @@
  * and open the template in the editor.
  */
 package unidad4;
-
+import java.util.Scanner;
 /**
  *
  * @author marfonjul
  */
 public class boletin4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.err.println("ingrese un tamaño de vetor");
+        String tamano = sc.nextLine();
+        int[] vector = new int[tamano];
+
+        // Rellenar el vector con valores ficticios
+        for (int i = 0; i < tamano; i++) {
+            // Aquí puedes generar valores ficticios según tus necesidades
+            vector[i] = i * 2;
+        }
+
+        // Imprimir el contenido del vector en una línea
+        System.out.print("Contenido del vector: ");
+        for (int i = 0; i < tamano; i++) {
+            System.out.print(vector[i]);
+
+            // Agregar un espacio si no es el último elemento
+            if (i < tamano - 1) {
+                System.out.print(" ");
+            }
+        }
+
     
+    }
+ 
 }
--- Programacion/src/unidad4/teoria/teoria.java
+++ Programacion/src/unidad4/teoria/teoria.java
@@ -40,20 +40,18 @@ public class Teoria {
             default:
         }
         System.out.println("el estado de salud es :" + estado);
-    }
+    
     //arrays multidimensionales
-    double tabla1 [][] = new  double [3][2]; // 3 filas y dos columnas
-    int tabla2 [][] = new int [2][]; //2 filas y varias columnas
-    tabla2[0] = new int[2]; //primera fila con 2 columnas
-    tabla2[1] = new int[3]; //segunda fila con 3 columnas
-    
-    System.out.println(tabla1.length);//nº de filas
-    System.out.println(tabla2.length);//nº de filas
-    System.out.println(tabla2[0].length);//nº de columnas
-    System.out.println(tabla2[1].length);//nº de columnas
-    
+    double tabla1 [][] = new double[3][2];
+    int tabla2[][] = new int[2][];
+    tabla2[0] = new int[2];
+    tabla2[1] = new int[3];
+    System.out.println(tabla1.length); // 3 (número de filas)
+    System.out.println(tabla2.length); // 2 (número de filas)
+    System.out.println(tabla2[0].length); // 2 (número de columnas)
+    System.out.println(tabla2[1].length); // 3 (número de columnas)
     //otro ejemplo
-    private int sumaCol(int[][] matriz, int columna){
+    private  int sumaCol(int[][] matriz, int columna){
         int suma = 0;
         for (int fila = 0; fila < matriz.length; fila++){
             suma += matriz[fila][columna];
@@ -61,4 +59,47 @@ public class Teoria {
         }
         return 0;
     }
+//inicializar una matriz
+    //matriz de 3 filas y 3 columnas
+    int[][] matriz ={ {0,0,0}, {1,1,1}, {2,2,2}};
+    // El siguiente ejemplo imprime por pantalla el contenido de una matriz:
+     for (int fila = 0; fila<matriz.length; fila++){
+    for (int col=0; col<matriz[fila].length; col++){
+        System.err.println(fila +":" + col + ":" + matriz[fila][col]);
+    }
+}
+    //alternativamente
+    //matriz[0][0] = 0;
+    //matriz[0][1] = 0;
+    //matriz[0][2] = 0;
+    //matriz[1][0] = 0;
+    //matriz[1][1] = 0;
+    //matriz[1][2] = 0;
+    //matriz[2][0] = 0;
+    //matriz[2][1] = 0;
+    //matriz[2][2] = 0;
+    
+    //código que rellenase el cubo con valores aleatorios, podría ser el
+//siguiente
+ int [][][][] cubo = new int [4][4][4][4];
+ for (int x = 0; x<cubo.length; x++){
+    for (int y = 0; y<cubo[x].length; y++){
+        for (int z=0; z<cubo[x][y].length; z++){}
+          //  cubo[x][y][z] = (int)(Math.random()* 6);
+    }
+}
+ 
+    public static void main(String[] args) {
+        int[] enteros = {1, 2, 3};
+        prueba(enteros);
+        for(int i=0; i<enteros.length; i++){
+            System.out.println(enteros[i]);
+        }
+        public void prueba(int [] enteros){
+            for(int i=0; i<enteros.length; i++){
+                enteros[i] *=2;
+            }
+        }
+    }
+}
 }
