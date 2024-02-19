08d9fc9a0e2f3b54b071a7cf6fbf27c9fa074f88
IESPIRINGALLA\marfonjul <juliamartinezfontao@gmail.com>
2024-02-19 19:22:45 +0100

    ..

---
+++ Programacion/src/unidad3/repaso/Repaso.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package unidad3;
+
+/**
+ *
+ * @author marfonjul
+ */
+public class Repaso {
+public static void main(String[]args){
+String imprimir = new Srting("sumar 5 numeros enteros");
+String imprimir; 
+
+
+}
+private static void imprimeTablaSuma(int suma){
+
+}
+}
