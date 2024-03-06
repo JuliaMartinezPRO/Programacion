1b0bcf22b848647894943ceeb3a2bb29c258c61d
IESPIRINGALLA\marfonjul <juliamartinezfontao@gmail.com>
2024-03-06 19:50:30 +0100

    ...

--- Programacion/src/unidad4/repaso/Mapas.java
+++ Programacion/src/unidad4/repaso/Mapas.java
@@ -22,7 +22,7 @@ public class Mapas {
         //comprueba si puede contener nulos en las claves o en los valores 
         //que ocurre si dos alumnos se llaman exactamente igual ? explicalo
         HashMap <Persona, Integer> map = new HashMap<>();
-        Pers
+        
         
         
         
